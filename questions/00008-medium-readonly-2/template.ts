f8027abefe6781490a168c3b340fc00af1568dd4
chendiyun <864423284@qq.com>
2024-06-11 20:25:21 +0800

    00008 03057 03060 03312

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,7 @@
-type MyReadonly2<T, K> = any
+type MyExclude2<T,K> = T extends K ? never : T
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [k in K]: T[k]
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P]
+}
